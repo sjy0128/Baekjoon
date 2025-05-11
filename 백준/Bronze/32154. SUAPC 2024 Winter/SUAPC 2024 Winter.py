21d644f35de55fc61eaafaa616eb90b6c175b940
sjy0128 <sjunyung1496@naver.com>
2025-05-11 23:51:21 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,4 @@
+q = ["ABCDEFGHJLM","ACEFGHILM","ACEFGHILM","ABCEFGHLM","ACEFGHLM",
+     "ACEFGHLM","ACEFGHLM","ACEFGHLM","ACEFGHLM","ABCFGHLM"]
+n = int(input())-1
+print(len(q[n]), ' '.join(q[n]), sep='\n')
