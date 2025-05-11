4e17ff39b01d247977854991fb533d06f1b69f06
sjy0128 <sjunyung1496@naver.com>
2025-05-11 23:37:00 +0900

    [Bronze IV] Title: Schronisko, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8760. Schronisko/Schronisko.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int z, w, k;
+    scanf("%d", &z);
+    for(int i=0; i<z; i++) {
+        scanf("%d %d", &w, &k);
+        printf("%d\n", w*k/2);
+    }
+    return 0;
+}
