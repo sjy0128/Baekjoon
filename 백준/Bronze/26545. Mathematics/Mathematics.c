7b3f7f31999a9dbd98f336af297002d1250080d1
sjy0128 <sjunyung1496@naver.com>
2025-05-11 23:59:58 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int n, temp, sum=0;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++) {
+        scanf("%d", &temp);
+        sum += temp;
+    }
+    printf("%d", sum);
+    
+    return 0;
+}
