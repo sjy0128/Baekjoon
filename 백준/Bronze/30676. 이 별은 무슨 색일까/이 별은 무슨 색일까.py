4883830414079ec58acac0f9a1d57e81e9877861
sjy0128 <sjunyung1496@naver.com>
2025-05-11 23:52:22 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+lam = int(input())
+
+if 780 >= lam >= 620:
+    print("Red")
+elif lam >= 590:
+    print("Orange")
+elif lam >= 570:
+    print("Yellow")
+elif lam >= 495:
+    print("Green")
+elif lam >= 450:
+    print("Blue")
+elif lam >= 425:
+    print("Indigo")
+elif lam >= 380:
+    print("Violet")
