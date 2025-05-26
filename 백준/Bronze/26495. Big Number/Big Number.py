d6d41caa08bf2f560523968de17fb22f8a0d30b0
sjy0128 <sjunyung1496@naver.com>
2025-05-26 18:50:05 +0900

    [Bronze IV] Title: Big Number, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/26495. Big Number/Big Number.py
@@ -0,0 +1,63 @@
+a = input()
+for i in range(len(a)):
+    if a[i] == '0':
+        print("0000")
+        print("0  0")
+        print("0  0")
+        print("0  0")
+        print("0000")
+    elif a[i] == '1':
+        print("   1")
+        print("   1")
+        print("   1")
+        print("   1")
+        print("   1")
+    elif a[i] == '2':
+        print("2222")
+        print("   2")
+        print("2222")
+        print("2")
+        print("2222")
+    elif a[i] == '3':
+        print("3333")
+        print("   3")
+        print("3333")
+        print("   3")
+        print("3333")
+    elif a[i] == '4':
+        print("4  4")
+        print("4  4")
+        print("4444")
+        print("   4")
+        print("   4")
+    elif a[i] == '5':
+        print("5555")
+        print("5")
+        print("5555")
+        print("   5")
+        print("5555")
+    elif a[i] == '6':
+        print("6666")
+        print("6")
+        print("6666")
+        print("6  6")
+        print("6666")
+    elif a[i] == '7':
+        print("7777")
+        print("   7")
+        print("   7")
+        print("   7")
+        print("   7")
+    elif a[i] == '8':
+        print("8888")
+        print("8  8")
+        print("8888")
+        print("8  8")
+        print("8888")
+    elif a[i] == '9':
+        print("9999")
+        print("9  9")
+        print("9999")
+        print("   9")
+        print("   9")
+    print()
