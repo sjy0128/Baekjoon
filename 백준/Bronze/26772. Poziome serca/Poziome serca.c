d1d257250ad71dc3674ff51b798210934c61f796
sjy0128 <sjunyung1496@naver.com>
2025-05-27 08:26:18 +0900

    [Bronze IV] Title: Poziome serca, Time: 96 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    for(i=0; i<n; i++) printf(" @@@   @@@  ");
+    printf("\n");
+    for(i=0; i<n; i++) printf("@   @ @   @ ");
+    printf("\n");
+    for(i=0; i<n; i++) printf("@    @    @ ");
+    printf("\n");
+    for(i=0; i<n; i++) printf("@         @ ");
+    printf("\n");
+    for(i=0; i<n; i++) printf(" @       @  ");
+    printf("\n");
+    for(i=0; i<n; i++) printf("  @     @   ");
+    printf("\n");
+    for(i=0; i<n; i++) printf("   @   @    ");
+    printf("\n");
+    for(i=0; i<n; i++) printf("    @ @     ");
+    printf("\n");
+    for(i=0; i<n; i++) printf("     @      ");
+    
+    
+    return 0;
+}
