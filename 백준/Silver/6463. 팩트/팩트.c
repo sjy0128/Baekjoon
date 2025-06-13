c7842aff2b7a9db47a01dec7d8778ef63d054c0c
sjy0128 <sjunyung1496@naver.com>
2025-06-13 23:07:02 +0900

    [Silver V] Title: 팩트, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/6463. 팩트/팩트.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int f1(int a), f2(int a);
+
+int main() {
+    int n, i;
+    while(scanf("%d", &n) != EOF) {
+        printf("%5d -> %d\n", n, f2(f1(n))%10);
+    }
+    return 0;
+}
+
+int f1(int a) {
+    if(a <= 1) return 1;
+    int b = f1(a - 1);
+    return (f2(a) % 100000) * (f2(b) % 100000);
+}
+
+int f2(int a) {
+    if(a % 10 != 0) return a;
+    return f2(a / 10);
+}
