4d2192e59e2d65f93213ebb73233de9c03631c55
sjy0128 <sjunyung1496@naver.com>
2025-07-15 23:30:30 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    int n, i, j;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++) {
+        for(j=0; j<n-i; j++) {
+            printf(" ");
+        }
+        for(j=0; j<i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=n-1; i>=1; i--) {
+        for(j=0; j<n-i; j++) {
+            printf(" ");
+        }
+        for(j=0; j<i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
