72124fa1c1559ca5f8dbe42e8cac177ec4b40c12
sjy0128 <sjunyung1496@naver.com>
2025-08-07 18:13:04 +0900

    [Silver III] Title: N과 M (5), Time: 36 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int n, m, a[8], v[8], r[8];
+
+void f(int d);
+
+int main(void) {
+    scanf("%d %d", &n, &m);
+    for(int i=0; i<n; i++) scanf("%d", &a[i]);
+    for(int i=0; i<n-1; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(a[i] > a[j]) {
+                a[i] ^= a[j] ^= a[i] ^= a[j];
+            }
+        }
+    }
+    f(0);
+    return 0;
+}
+
+void f(int d) {
+    if(d == m) {
+        for(int i=0; i<m; i++) printf("%d ", r[i]);
+        printf("\n");
+        return;
+    }
+    for(int i=0; i<n; i++) {
+        if(!v[i]) {
+            r[d] = a[i];
+            v[i] = 1;
+            f(d+1);
+            v[i] = 0;
+        }
+    }
+}
