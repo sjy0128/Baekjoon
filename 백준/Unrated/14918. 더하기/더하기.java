7469235f809fe91659f905a3b4cb893f5c1f58f5
sjy0128 <sjunyung1496@naver.com>
2025-08-26 23:41:22 +0900

    [Unrated] Title: 더하기, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+	    Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		System.out.print(a + b);
+	}
+}
