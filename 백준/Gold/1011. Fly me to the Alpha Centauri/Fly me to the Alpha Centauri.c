34fc4395a3ffd69ab572a9b2035510d2803deca8
sjy0128 <sjunyung1496@naver.com>
2025-12-23 20:51:46 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int f(unsigned int n);
+
+int main(void) {
+    int t, x, y;
+    scanf("%d", &t);
+    while(t--) {
+        scanf("%d %d", &x, &y);
+        printf("%d\n", f(y-x));
+    }
+    return 0;
+}
+
+int f(unsigned int n) {
+    int a = 1, r = 0;
+    long long sum = 0;
+    while(1) {
+        if(sum + a * 2 <= n) {
+            sum += a * 2;
+            a++;
+            r += 2;
+        } else if(sum + a < n) {
+            sum += a;
+            a++;
+            r++;
+        } else break;
+    }
+    return r + (sum != n);
+}
