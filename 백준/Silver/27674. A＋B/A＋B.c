add19760bce3259fede652d31afa20a7d124de74
sjy0128 <sjunyung1496@naver.com>
2026-01-29 18:42:57 +0900

    [Silver V] Title: A+B, Time: 0 ms, Memory: 1112 KB, Score: 1 point -BaekjoonHub

---
+++ 백준/Silver/27674. A＋B/A＋B.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int t, l, c, d[10];
+    long long sum;
+    char s[13];
+    scanf("%d", &t);
+    while(t--) {
+        scanf("%s", s);
+        for(int i = 0; i < 10; i++) d[i] = 0;
+        for(l = 0; s[l] != '\0'; l++) d[s[l]-'0']++;
+        sum = 0, c = 9;
+        while(l) {
+            while(!d[c]) c--;
+            sum = sum * (l > 1 ? 10 : 1) + c;
+            l--, d[c]--;
+        }
+        printf("%lld\n", sum);
+    }
+    return 0;
+}
