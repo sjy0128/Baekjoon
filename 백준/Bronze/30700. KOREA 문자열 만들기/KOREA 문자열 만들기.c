1478e38df16cd0042eea69e03e16b55851e57035
sjy0128 <sjunyung1496@naver.com>
2026-02-18 21:32:36 +0900

    [Bronze II] Title: KOREA 문자열 만들기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/30700. KOREA 문자열 만들기/KOREA 문자열 만들기.c
+++ 백준/Bronze/30700. KOREA 문자열 만들기/KOREA 문자열 만들기.c
@@ -1,7 +1,2 @@
-#include <stdio.h>
-int i,a;
-int main(){
-char c,*k="KOREA";
-while((c=getc(stdin))!=EOF)a+=c==k[i]?(i=++i%5)-i+1:0;
-printf("%d",a);
-}
+#include<stdio.h>
+int i,a;int main(){char c,*k="KOREA";while((c=getc(stdin))!=EOF)a+=c==k[i]?1+0*(i=++i%5):0;printf("%d",a);}
