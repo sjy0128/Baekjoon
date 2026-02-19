0cec8a4feb293047e139083397f9580055912aa7
sjy0128 <sjunyung1496@naver.com>
2026-02-19 22:58:06 +0900

    [Bronze III] Title: 대지, Time: 80 ms, Memory: 36352 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,6 +1,8 @@
+import sys
+i = sys.stdin.readline
 x, y = [], []
-for _ in [0] * int(input()):
-    a, b = map(int, input().split())
+for _ in [0] * int(i()):
+    a, b = map(int, i().split())
     x.append(a)
     y.append(b)
 print((max(x) - min(x)) * (max(y) - min(y)))
