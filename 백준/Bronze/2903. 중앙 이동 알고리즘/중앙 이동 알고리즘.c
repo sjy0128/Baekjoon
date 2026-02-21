2d4f24f6e5639e8ce3c25d282f43f47c4566599a
sjy0128 <sjunyung1496@naver.com>
2026-02-21 15:11:18 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.c
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.c
@@ -1,9 +1,1 @@
-#include <stdio.h>
-
-int main(void) {
-    int n, r_ans = 2;
-    scanf("%d", &n);
-    while(n--) r_ans = r_ans * 2 - 1;
-    printf("%d", r_ans * r_ans);
-    return 0;
-}
+main(n,a){scanf("%d",&n);a=(1<<n)+1;printf("%d",a*a);}
