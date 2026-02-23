0cc6fda7aacdf4bff81c1ccfd9c0d02a8eff7f1a
sjy0128 <sjunyung1496@naver.com>
2026-02-23 15:33:50 +0900

    [Platinum V] Title: 숫자 만들기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Platinum/1511. 숫자 만들기/숫자 만들기.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define MAX_LEN 50
+#define REPF(i, n) for(int i = 0; i < (n); i++)
+#define REPB(i, n) for(int i = (n) - 1; i >= 0; i--)
+
+int main(void) {
+    int cards[10], n = 0, prev = -1, next;
+    int ans[MAX_LEN], ai = 0, pad = 0;
+    
+    REPF(i, 10) {
+        scanf("%d", &cards[i]);
+        n += cards[i];
+    }
+    
+    if(n == cards[0]) {
+        printf("0");
+        return 0;
+    }
+    
+    REPF(i, n+1) ans[i] = -1;
+    
+    while(n) {
+        next = -1;
+        
+        REPB(i, 10) {
+            if(i == prev) continue;
+            
+            if(next == -1 && cards[i]) next = i;
+            
+            if(cards[i] > n/2) {
+                next = i;
+                break;
+            }
+        }
+        
+        if(next == -1) break;
+        
+        cards[prev = ans[ai++] = next]--;
+        n--;
+    }
+    
+    while(!ans[pad]) pad++;
+    
+    REPF(i, ai-pad) printf("%d", ans[i+pad]);
+    
+    return 0;
+}
